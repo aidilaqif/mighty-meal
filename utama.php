1c80347d5a06c1978a60f8fa3c8102def1e5e5cd
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-06-14 17:50:53 +0800

    Update utama.php

--- utama.php
+++ utama.php
@@ -1,2 +1,24 @@
-<h1>Utama</h1>
-<p>This is the content for Utama.</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Utama</title>
+    <link rel="stylesheet" href="styles/utama.css">
+</head>
+<body>
+    <div class="selamat-datang">
+        <img src="image/header-footer/Utama.png" alt="Selamat Datang!">
+    </div>
+    <div class="messages-to-read">
+        <img src="image/utama/messages-to-read.PNG" alt="messages to read">
+    </div>
+    <div class="jumlah-keperluan-tenaga.png">
+        <img src="image/utama/jumlah-keperluan-tenaga.PNG" alt="jumlah keperluan tenaga">
+    </div>
+    <div class="goal">
+        <p>The goal of this event is to bring leaders in information technology together from across the globe.</p>
+    </div>
+    div
+</body>
+</html>
